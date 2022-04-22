a0df2fe3fabf78c30ede562586943b49eaef22de
Inferno <43pradeep07@gmail.com>
2022-04-22 22:08:51 +0530

    SST

---
+++ Sasta Shark Tank/sastasharktank.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b;
+        cin>>a>>b;
+        if(a*10 > b*5)
+          cout<<"FIRST"<<endl;
+        else if(a*10 == b*5)
+          cout<<"ANY"<<endl;
+        else
+          cout<<"SECOND"<<endl;
+    }
+    
+    return 0;
+}
