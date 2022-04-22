9b3326d292e423db52ee4b9f90d7f50131c9e63b
Inferno <43pradeep07@gmail.com>
2022-04-22 19:58:40 +0530

    SIXFRIENDS

---
+++ Six Friends/sixfriends.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; 
+    cin>>t;
+    while(t--){
+        int x, y;
+        cin>>x>>y;
+        if((x*3) <= (y*2))
+          cout<<(x*3)<<endl;
+        else
+          cout<<(y*2)<<endl;
+    }
+    
+    return 0;
+}
